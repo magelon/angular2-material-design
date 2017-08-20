546d2b732c6c67660605708805ac8b17a7395c0e
lon <alfardproject@gmail.com>
2017-08-19 22:12:41 -0700

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
+import 'hammerjs';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
